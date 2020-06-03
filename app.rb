4dd4f46167644313c49247e1e915c7e491c74856
matiaswiegandt <matias.wiegandt@kellogg.northwestern.edu>
2020-06-03 14:17:53 +0000

    so far so good

--- app.rb
+++ app.rb
@@ -15,5 +15,74 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-# events_table = DB.from(:events)
-# rsvps_table = DB.from(:rsvps)
+flights_table = DB.from(:flights)
+bookings_table = DB.from(:bookings)
+users_table = DB.from(:users)
+seats_table = DB.from(:seats)
+
+before do
+    # SELECT * FROM users WHERE id = session[:user_id]
+    @current_user = users_table.where(:id => session[:user_id]).to_a[0]
+    puts @current_user.inspect
+end
+
+# flight selection page (all flights) **First page of this app thing
+get "/" do
+    # before stuff runs
+    @flights = flights_table.all
+    view "flights" #Sends it to "flights" which is the erb(html) file of that screen
+end
+
+get "/flights/:id" do
+     @flight = flights_table.where(:id => params["id"]).to_a[0]
+    view "flight"
+end
+
+# Form to create a new user
+get "/users/new" do
+    view "new_user"
+end
+
+# Receiving end of new user form
+post "/users/create" do
+    users_table.insert(:name => params["name"],
+                       :email => params["email"],
+                       :password => params["password"],
+                       :credit_number => params["credit_number"],
+                       :credit_exp_date => params["credit_exp_date"],
+                       :credit_security => params["credit_security"])
+    view "create_user"
+end
+
+# Form to login
+get "/logins/new" do
+    view "new_login"
+end
+
+# Receiving end of login form
+post "/logins/create" do
+    puts params
+    email_entered = params["email"]
+    password_entered = params["password"]
+    # SELECT * FROM users WHERE email = email_entered
+    user = users_table.where(:email => email_entered).to_a[0]
+    if user
+        puts user.inspect
+        # test the password against the one in the users table
+        if user[:password] == password_entered
+            session[:user_id] = user[:id]
+            view "create_login"
+        else
+            view "create_login_failed"
+        end
+    else 
+        view "create_login_failed"
+    end
+end
+
+# Logout
+get "/logout" do
+    view "logout"
+end
+
+
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,65 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :flights do
   primary_key :id
-  String :title
-  String :description, text: true
+  String :flight_num
   String :date
-  String :location
+  String :dep_time
+  String :arr_time
+  String :price
+  String :origin
+  String :destination
 end
-DB.create_table! :rsvps do
+DB.create_table! :bookings do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
+  foreign_key :flight_id
+  foreign_key :user_id
+  foreign_key :seat_chosen
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
+  String :credit_number
+  String :credit_exp_date
+  String :credit_security
+end
+DB.create_table! :seats do
+  primary_key :id
+  foreign_key :booking_id
+  String :seat_number
+  Boolean :booked
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+flights_table = DB.from(:flights)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+flights_table.insert(flight_num: "1",
+                    date: "June 21", 
+                    dep_time: "8:00 AM",
+                    arr_time: "9:30 AM",
+                    price: "$ 1,200 MXN",
+                    origin: "Mexico",
+                    destination: "Monterrey")
+                    
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+flights_table.insert(flight_num: "2",
+                    date: "June 21", 
+                    dep_time: "11:00 AM",
+                    arr_time: "12:30 PM",
+                    price: "$ 1,400 MXN",
+                    origin: "Mexico",
+                    destination: "Monterrey")
+
+seats_table = DB.from(:seats)
+
+seats_table.insert(seat_number: "1a")
+seats_table.insert(seat_number: "1b")
+seats_table.insert(seat_number: "2a")
+seats_table.insert(seat_number: "2b")
+seats_table.insert(seat_number: "3a")
+seats_table.insert(seat_number: "3b")
+seats_table.insert(seat_number: "4a")
+seats_table.insert(seat_number: "4b")
